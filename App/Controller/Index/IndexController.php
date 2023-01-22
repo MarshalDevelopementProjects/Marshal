58d6499b711e4802c558b01a4eb5375a0a278d19
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-22 15:17:29 +0530

    Updateed version of the index page controller with user login and already logged in support

--- App/Controller/Index/IndexController.php
+++ App/Controller/Index/IndexController.php
@@ -4,12 +4,22 @@ namespace App\Controller\Index;
 
 require __DIR__ . "/../../../vendor/autoload.php";
 
-use App\Controller\Controller;
+use App\Controller\Authenticate\UserAuthController;
 
-class IndexController extends Controller
+class IndexController extends UserAuthController
 {
-    public function defaultAction()
-    {
-      $this->sendResponse(view: "/landing.html", status: "success"); 
-    }
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function defaultAction(Object|array|string|int $optional = null)
+  {
+    if (!parent::isLogged())
+      $this->sendResponse(view: "/landing.html", status: "success");
+    else
+      header("Location: http://localhost/public/user/dashboard");
+    exit;
+  }
 }
