f57fc88285ee402ef54523c97c69fbfd8aabc752
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-22 15:20:49 +0530

    new controller of the Client actor

---
+++ App/Controller/Client/ClientController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Client;
+
+use App\Controller\Authenticate\UserAuthController;
+use App\Controller\User\UserController;
+use App\Model\Client;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class ClientController extends UserController
+{
+    private Client $client;
+
+    public function __construct()
+    {
+        try {
+            parent::__construct();
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function defaultAction(Object|array|string|int $optional = null)
+    {
+    }
+
+    public function auth()
+    {
+        return parent::auth();
+    }
+}
