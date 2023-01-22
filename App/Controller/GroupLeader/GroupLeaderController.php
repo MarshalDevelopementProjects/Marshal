ea978f2332937c9ebe6c8f2b06e2703fafc5bb0f
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-22 15:26:01 +0530

    Added the basic class structure of the GroupLeaderController

---
+++ App/Controller/GroupLeader/GroupLeaderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\GroupLeader;
+
+use App\Controller\Authenticate\UserAuthController;
+use App\Controller\ProjectMember\ProjectMemberController;
+use App\Model\GroupLeader;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class GroupLeaderController extends ProjectMemberController
+{
+    private GroupLeader $groupLeader;
+
+    public function __construct()
+    {
+        try {
+            parent::__construct();
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function defaultAction(Object|array|string|int $optional = null)
+    {
+    }
+
+    public function auth()
+    {
+        return parent::auth();
+    }
+}
