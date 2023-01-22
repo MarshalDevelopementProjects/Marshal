dc890f1a4832629de47f5fe53d29bc58037cbf11
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-22 15:13:32 +0530

    New rendering function to render HTML files

---
+++ Core/View.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+class View
+{
+    public static function render(string|int $view, array $args = [])
+    {
+        // access the view files relative to Core directory 
+        $file = __DIR__ . '/../View/src';
+        if (is_string($view)) {
+            $file .= $view;
+        } else if (is_numeric($view)) {
+            $file .= "$view.html";
+        }
+
+        if (is_readable($file)) {
+            $data = json_encode($args);
+            require $file;
+        } else {
+            $data = json_encode($args);
+            require __DIR__ . '/../View/src/errors/500.html';
+        }
+    }
+}
