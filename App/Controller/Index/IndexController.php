72b0af6da9dede2dc01394f479556ab2b423b356
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-23 21:48:10 +0530

    added user login auth fuctionality to redirect users to their landing pages if they are logged in

--- App/Controller/Index/IndexController.php
+++ App/Controller/Index/IndexController.php
@@ -16,10 +16,14 @@ class IndexController extends UserAuthController
 
   public function defaultAction(Object|array|string|int $optional = null)
   {
-    if (!parent::isLogged())
+    if (parent::isLogged()) {
+      if ((parent::getCredentials()->primary_role == "admin")) {
+        header("Location: http://localhost/public/admin/dashboard");
+      } else {
+        header("Location: http://localhost/public/user/dashboard");
+      }
+    } else
       $this->sendResponse(view: "/landing.html", status: "success");
-    else
-      header("Location: http://localhost/public/user/dashboard");
     exit;
   }
 }
