b76d367eb0046bb89367f266050279bcc19ab123
Harsha Gunawardane <mrguawardane@gmail.com>
2023-01-24 21:52:14 +0530

    Add construct and create task function

--- App/Model/Task.php
+++ App/Model/Task.php
@@ -8,4 +8,25 @@ use App\CrudUtil\CrudUtil;
 
 class Task implements Model
 {
+    private CrudUtil $crud_util;
+    public function __construct(){
+        try {
+            $this->crud_util = new CrudUtil();
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function createTask(array $args = array()){
+        $sql_string = "INSERT INTO task (`project_id`, `description`, `deadline`, `task_name`) VALUES (:project_id, :description, :deadline, :task_name)";
+        
+        try {
+            $this->crud_util->execute($sql_string, $args);
+            
+            return true;
+        } catch (\Exception $exception) {
+            return false;
+        }
+    }
+
 }
