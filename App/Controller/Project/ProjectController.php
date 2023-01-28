c0c892768cd28a6fe2d901ffe4e5c6952ee30342
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-28 08:10:27 +0530

    extended project controller with the controller base class

--- App/Controller/Project/ProjectController.php
+++ App/Controller/Project/ProjectController.php
@@ -3,13 +3,13 @@
 namespace App\Controller\Project;
 
 use App\Controller\Authenticate\UserAuthController;
-use App\Controller\User\UserController;
+use App\Controller\Controller;
 use App\Model\Project;
 use Core\Validator\Validator;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-class ProjectController extends UserController
+class ProjectController extends Controller
 {
     public function defaultAction(Object|array|string|int $optional = null)
     {
