e9b0cb636c26492d993a4df5ecdb8d781a13cbcf
TDMWalpala <tharindudarshana2019@gmail.com>
2023-01-28 20:58:03 +0530

    updated the admin model to support new controller functionalities

--- App/Model/Admin.php
+++ App/Model/Admin.php
@@ -8,7 +8,6 @@ use App\CrudUtil\CrudUtil;
 
 class Admin implements Model
 {
-
     private $crud_util;
     private $admin_data;
     private $query_results;
@@ -29,7 +28,7 @@ class Admin implements Model
 
     // this will only be used for testing since admins aren't allowed
     // create other administrators
-    public function createAdmin(array $args = array())
+    private function createAdmin(array $args = array())
     {
         if (!empty($args)) {
             $sql_string = "INSERT INTO `admin` (`id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `street_address`, `city`, `country`, `phone_number`)
@@ -71,16 +70,59 @@ class Admin implements Model
     // create a single user
     public function createUser(array $args = array())
     {
+        if (!empty($args)) {
+            $sql_string = "INSERT INTO `user`(`username`, `first_name`, `last_name`, `email_address`, `password`, `phone_number`)
+                           VALUES (:username, :first_name, :last_name, :email_address, :password, :phone_number)";
+            $args['password'] = password_hash($args['password'], PASSWORD_ARGON2ID);
+            try {
+                $this->crud_util->execute($sql_string, $args);
+                return true;
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
+        }
+        return false;
     }
 
     // create more than one user in one go
     public function createUsers(array $args = array())
     {
+        foreach ($args as $user) {
+            if (!empty($user)) {
+                $sql_string = "INSERT INTO `user`(`username`, `first_name`, `last_name`, `email_address`, `password`, `phone_number`)
+                           VALUES (:username, :first_name, :last_name, :email_address, :password, :phone_number)";
+                $user['password'] = password_hash($user['password'], PASSWORD_ARGON2ID);
+                try {
+                    $this->crud_util->execute($sql_string, $user);
+                    return true;
+                } catch (\Exception $exception) {
+                    throw $exception;
+                }
+            }
+            return false;
+        }
     }
 
     // update a single user
-    public function updateUser(string|int $userID, array $params = array())
+    public function updateUserDetails(string|int $id, array $args = array())
     {
+        try {
+            $sql_string = "UPDATE `user` SET
+                          `username` = :username,
+                          `first_name` = :first_name,
+                          `last_name` = :last_name,
+                          `email_address` = :email_address,
+                          `phone_number` = :phone_number,
+                          `position` = :position,
+                          `bio` = :bio,
+                          `user_status` = :user_status
+                          WHERE `id` = :id";
+            $args["id"] = $id;
+            $this->crud_util->execute($sql_string, $args);
+            return true;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
     // read a single user in the system 
@@ -92,28 +134,7 @@ class Admin implements Model
             try {
                 $result = $this->crud_util->execute($sql_string, [$key => $value]);
                 if ($result->getCount() > 0) {
-                    $this->admin_data = $result->getResults();
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (\Exception $exception) {
-                throw $exception;
-            }
-        }
-        return false;
-    }
-
-    // read a single user in the system 
-    public function readUserByUsername(string|int $username)
-    {
-        if ($username) {
-            $sql_string = "SELECT * FROM `user` WHERE `username` = :username";
-            // example format => "SELECT * FROM users WHERE id = :id";
-            try {
-                $result = $this->crud_util->execute($sql_string, ["username" => $username]);
-                if ($result->getCount() > 0) {
-                    $this->admin_data = $result->getResults();
+                    $this->query_results = $result->getResults();
                     return true;
                 } else {
                     return false;
@@ -128,12 +149,11 @@ class Admin implements Model
     // read all the users in the system 
     public function readAllUsers()
     {
-        $sql_string = "SELECT * FROM `user`";
-        // example format => "SELECT * FROM users WHERE id = :id";
+        $sql_string = "SELECT `id`, `username`, `email_address` FROM `user`";
         try {
             $result = $this->crud_util->execute($sql_string);
             if ($result->getCount() > 0) {
-                $this->admin_data = $result->getResults();
+                $this->query_results = $result->getResults();
                 return true;
             } else {
                 return false;
@@ -146,28 +166,91 @@ class Admin implements Model
     // this function will be used for searching table with conditions
     // mainly operators and all greater than some values and so on
     // mainly used for filtering
-    public function conditionalSearch(string $table, string|int|array $fields, string|array $operators)
+    public function conditionalSearch(string $table = "user", string|int|array $fields, string|array $operators)
     {
     }
 
-    // delete a particular user
-    public function deleteUser(string|int|array $params)
+    public function getActiveUsers()
     {
+        $sql_string = "SELECT `id`, `username`, `email_address` FROM `user` WHERE `user_state` = 'ONLINE'";
+        try {
+            $result = $this->crud_util->execute($sql_string);
+            if ($result->getCount() > 0) {
+                $this->query_results = $result->getResults();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
-    // delete all the users that fall under a certain condition
-    public function conditionalDelete(string|int|array $params)
+    public function getBlockedUsers()
     {
+        $sql_string = "SELECT `id`, `username`, `email_address` FROM `user` WHERE `access` = 'DISABLED'";
+        try {
+            $result = $this->crud_util->execute($sql_string);
+            if ($result->getCount() > 0) {
+                $this->query_results = $result->getResults();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
-    // update a single user using the user id
-    public function update(string|int $id, array $args = array())
+    // disable a particular user account
+    public function disableUserAccount(string $key, string|int $value)
     {
+        try {
+            $sql_string = "UPDATE `user` SET
+                          `access` = :access
+                          WHERE `$key` = :$key";
+            $this->crud_util->execute($sql_string, ["access" => "DISABLED", $key => $value]);
+            return true;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
-    // update a single user using the user id
-    public function conditionalUpdate(string|int $id, array $args = array())
+    // enable a particular user account
+    public function enableUserAccount(string $key, string|int $value)
     {
+        try {
+            $sql_string = "UPDATE `user` SET
+                          `access` = :access
+                          WHERE `$key` = :$key";
+            $this->crud_util->execute($sql_string, ["access" => "ENABLED", $key => $value]);
+            return true;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    // update admin details
+    private function updateAdminDetails(string|int $id, array $args = array())
+    {
+        try {
+            $sql_string = "UPDATE `admin` SET
+                          `username` = :username,
+                          `first_name` = :first_name,
+                          `last_name` = :last_name,
+                          `email_address` = :email_address,
+                          `phone_number` = :phone_number,
+                          `street_address` = :street_address,
+                          `city` = :city,
+                          `bio` = :bio,
+                          `country` = country:
+                          WHERE `id` = :id";
+            $args["id"] = $id;
+            $this->crud_util->execute($sql_string, $args);
+            return true;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
     public function getAdminData()
