9e32c12e8eaeb35c7f91f6ad26e5b5fb7a5d9d98
Harsha Gunawardane <mrgunawardane@gmail.com>
2023-01-29 17:23:50 +0530

    Add pickupTask function

--- App/Controller/ProjectMember/ProjectMemberController.php
+++ App/Controller/ProjectMember/ProjectMemberController.php
@@ -6,6 +6,7 @@ use App\Controller\Authenticate\UserAuthController;
 use App\Controller\User\UserController;
 use App\Model\ProjectMember;
 use App\Model\Project;
+use App\Model\Task;
 use Core\Validator\Validator;
 
 require __DIR__ . '/../../../vendor/autoload.php';
@@ -31,4 +32,40 @@ class ProjectMemberController extends UserController
     {
         return parent::auth();
     }
+
+    public function pickupTask(){
+
+        $data = json_decode(file_get_contents('php://input'));
+        $payload = $this->userAuth->getCredentials();
+        $project_id = $_SESSION["project_id"];
+        $user_id = $payload->id;
+
+        $args = array(
+            "status" => $data->state,
+            "memberId" => $user_id,
+            "project_id" => $project_id,
+            "task_name" => $data->task_name
+        );
+
+        $task = new Task();
+        $message = "";
+        try {
+            $task->pickupTask($args);
+            $message = "Successfully picked";
+
+            // send notification to leader
+            
+        } catch (\Throwable $th) {
+            $message = "Failed to pick up";
+        }
+
+        $this->sendJsonResponse(
+            status: "success",
+            content: [
+                "message" => $message
+            ]
+        );
+    }
+
+    
 }
