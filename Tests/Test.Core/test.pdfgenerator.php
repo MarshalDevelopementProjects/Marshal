53688c0da492dabee44ddd31d43da9f147355fe7
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-01-29 20:09:08 +0530

    test cases of the core\pdfgenerator

--- Tests/Test.Core/test.pdfgenerator.php
+++ Tests/Test.Core/test.pdfgenerator.php
@@ -2,8 +2,12 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use Core\PdfGenerator;
+use Core\PdfGenerator\PdfGenerator;
 
 function testPdfGenerator()
 {
+    $PdfGenerator = new PdfGenerator();
+    $PdfGenerator->renderPDF(content: "<h1>Test PDF</h1>", styles: "", file_name: "first_pdf.pdf");
 }
+
+testPdfGenerator();
