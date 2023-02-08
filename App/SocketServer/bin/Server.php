cb772172555a0b0399f7e7e7df5a331d87f93c28
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-08 21:48:37 +0530

    new socket server calling code(aka bin file) ;

--- App/SocketServer/bin/Server.php
+++ App/SocketServer/bin/Server.php
@@ -1,1 +1,9 @@
 <?php
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use App\SocketServer\src\MessageController;
+
+$app = new Ratchet\App();
+$app->route("/{category}", new MessageController(), array('*'));
+$app->run();
