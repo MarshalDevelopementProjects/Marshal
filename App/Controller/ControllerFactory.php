59bf92f01349e1aceb1598df913d120234a3f79e
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-08 21:44:20 +0530

    new controller factory to support the ws server

--- App/Controller/ControllerFactory.php
+++ App/Controller/ControllerFactory.php
@@ -2,7 +2,26 @@
 
 namespace App\Controller;
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\Controller\Controller;
+use App\Controller\Administrator\AdminController;
+use App\Controller\ProjectLeader\ProjectLeaderController;
+use App\Controller\ProjectMember\ProjectMemberController;
+use App\Controller\GroupLeader\GroupLeaderController;
+use App\Controller\GroupMember\GroupMemberController;
+
 class ControllerFactory
 {
-
+    public static function factory(string $controller): ?Controller
+    {
+        return match ($controller) {
+            "Admin" => new AdminController(),
+            "ProjectLeader" => new ProjectLeaderController(),
+            "ProjectMember" => new ProjectMemberController(),
+            "GroupLeader" => new GroupLeaderController(),
+            "GroupMember" => new GroupMemberController(),
+            default => null,
+        };
+    }
 }
