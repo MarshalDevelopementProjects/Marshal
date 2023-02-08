38e33b581201291d81c95a07b113d44bb663b2bd
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-08 21:59:04 +0530

    Need to be removed

--- Tests/Test.Core/test.serverscript.php
+++ Tests/Test.Core/test.serverscript.php
@@ -1,1 +1,12 @@
 <?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\SocketServer\SocketServer;
+
+function testServerScript(): void
+{
+    $server = new SocketServer();
+}
+
+testServerScript();
