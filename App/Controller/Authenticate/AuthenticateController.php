388de60b87e2c3ec56f7521c8fe19cf83cfc5f73
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-08 21:39:50 +0530

    new class to support duplicate code and credenttials fetcher for the websocket server

--- App/Controller/Authenticate/AuthenticateController.php
+++ App/Controller/Authenticate/AuthenticateController.php
@@ -2,7 +2,34 @@
 
 namespace App\Controller\Authenticate;
 
-interface AuthenticateController
-{
+use Core\Config;
+use Core\Cookie;
+use Core\Token;
 
+abstract class AuthenticateController extends Token
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    abstract static public function isLogged();
+    /*
+     * Function description
+     *
+     * take the data inside the token payload and return that data as a php object
+     */
+    public static function getCredentials()
+    {
+        if (Cookie::cookieExists(Config::getApiGlobal("remember")['access'])) {
+            return self::getTokenPayload(Cookie::getCookieData(Config::getApiGlobal("remember")['access']));
+        } else if (Cookie::cookieExists(Config::getApiGlobal("remember")['refresh'])) {
+            return self::getTokenPayload(Cookie::getCookieData(Config::getApiGlobal("remember")['refresh']));
+        } else {
+            return null;
+        }
+    }
+
+    // abstract public function login();
+    abstract public function logout();
 }
