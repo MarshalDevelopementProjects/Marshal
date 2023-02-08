5aa09217faf3e4dea13e302fd8d57b3ab39daea7
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-08 21:45:53 +0530

    redundant file needs more care

--- App/Controller/Notification/NotificationController.php
+++ App/Controller/Notification/NotificationController.php
@@ -2,7 +2,48 @@
 
 namespace App\Controller\Notification;
 
-class NotificationController
-{
+require  __DIR__ . '/../../../vendor/autoload.php';
 
+use Ratchet\ConnectionInterface;
+
+class NotificationController implements \Ratchet\MessageComponentInterface
+{
+    protected \SplObjectStorage $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function onOpen(ConnectionInterface $conn)
+    {
+        // TODO: Implement onOpen() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function onClose(ConnectionInterface $conn)
+    {
+        // TODO: Implement onClose() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        // TODO: Implement onError() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function onMessage(ConnectionInterface $from, $msg)
+    {
+        // TODO: Implement onMessage() method.
+    }
 }
