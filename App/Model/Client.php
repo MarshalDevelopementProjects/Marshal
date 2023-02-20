bcda14124d59b5c8635e78d84d55a0c86a31a6af
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-20 21:41:48 +0530

    added client message support functions for retreiving the messages and adding a new message to the feedback forum

--- App/Model/Client.php
+++ App/Model/Client.php
@@ -10,6 +10,7 @@ class Client implements Model
 {
     private CrudUtil $crud_util;
     private object|int $project_data;
+    private object|array $message_data;
 
     public function __construct(string|int $project_id = null)
     {
@@ -26,10 +27,63 @@ class Client implements Model
 
     public function saveProjectFeedbackMessage(string|int $id, string|int $project_id, string $msg)
     {
+        // add the message to the project feedback table as well as the messages table
+        try {
+            // add the message to the message table first
+            if (!empty($msg)) {
+                $sql_string = "INSERT INTO `message`(`sender_id`, `stamp`, `message_type`, `msg`) VALUES (:sender_id, :stamp, :message_type, :msg)";
+                $date_time = date('Y-m-d H:i:s');
+                $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time, "message_type" => "PROJECT_FEEDBACK_MESSAGE", "msg" => $msg));
+                if (!$this->crud_util->hasErrors()) {
+                    $sql_string = "SELECT `id` FROM `message` WHERE `sender_id` = :sender_id AND `stamp` = :stamp";
+                    $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time));
+                    if (!$this->crud_util->hasErrors()) {
+                        $message_id = $this->crud_util->getFirstResult()["id"];
+                        $sql_string = "INSERT INTO `project_feedback_message`(`message_id`, `project_id`) VALUES (:message_id, :project_id)";
+                        $this->crud_util->execute($sql_string, array(
+                            "message_id" => $message_id, "project_id" => $project_id
+                        ));
+                        if (!$this->crud_util->hasErrors()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            // then add the message to the project messages table
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
     public function getProjectFeedbackMessages(string|int $project_id)
     {
+        // get all the messages in the project feedback 
+        // get all the messages in a project forum
+        try {
+            // use a join between the messages table and the project table where ids are equal
+            try {
+                $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `project_feedback_message` WHERE `project_id` = :project_id)";
+                $this->crud_util->execute($sql_string, array("project_id" => $project_id));
+                if (!$this->crud_util->hasErrors()) {
+                    $this->message_data = $this->crud_util->getResults();
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
     public function getReportData(string|int $project_id)
@@ -59,8 +113,13 @@ class Client implements Model
         }
     }
 
-    public function getProjectData()
+    public function getProjectData(): array|object|null
     {
         return $this->project_data;
+    }
+
+    public function getMessageData(): array|object|null
+    {
+        return $this->message_data;
     }
 }
