dca3025ce4074064c3f035f78230e97af9f4fcd9
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-20 06:59:09 +0530

    added client model code for fetching project data

--- App/Model/Client.php
+++ App/Model/Client.php
@@ -11,16 +11,56 @@ class Client implements Model
     private CrudUtil $crud_util;
     private object|int $project_data;
 
-    public function __construct(string|int $project_id)
+    public function __construct(string|int $project_id = null)
     {
-        $this->crud_util = new CrudUtil();
+        try {
+            $this->crud_util = new CrudUtil();
+            // CAUTION if the project is not found an exception is thrown
+            if (!$this->readProjectData($project_id)) {
+                throw new \Exception("User cannot be found");
+            } // otherwise the project details can be viewed using project_data
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 
-    public function saveProjectFeedbackMessage(string|int $project_id, string $msg)
+    public function saveProjectFeedbackMessage(string|int $id, string|int $project_id, string $msg)
+    {
+    }
+
+    public function getProjectFeedbackMessages(string|int $project_id)
     {
     }
 
     public function getReportData(string|int $project_id)
     {
+        try {
+            throw new \Exception("Not implemented yet");
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function readProjectData(string|int $project_id): bool
+    {
+        try {
+            $sql_string = "SELECT * FROM `project` WHERE `id` = :id";
+            $args = array("project_id" => $project_id);
+            // execute the query
+            $result = $this->crud_util->execute($sql_string, $args);
+            if ($result->getCount() > 0) {
+                $this->project_data = $result->getResults(); // get all the results or just one result this is an array of objects
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getProjectData()
+    {
+        return $this->project_data;
     }
 }
