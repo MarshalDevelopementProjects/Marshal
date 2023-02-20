16bbbc94217546b2f8bb7ffbe03cd0b9d36f1269
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-20 07:52:15 +0530

    updated project leader model to support the new database changed and added message saving and retrieving methods

--- App/Model/ProjectLeader.php
+++ App/Model/ProjectLeader.php
@@ -30,6 +30,58 @@ class ProjectLeader implements Model
     {
         // add the message to the project feedback table as well as the messages table
         try {
+            // add the message to the message table first
+            if (!empty($msg)) {
+                $sql_string = "INSERT INTO `message`(`sender_id`, `stamp`, `message_type`, `msg`) VALUES (:sender_id, :stamp, :message_type, :msg)";
+                $date_time = date('Y-m-d H:i:s');
+                $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time, "message_type" => "PROJECT_FEEDBACK_MESSAGE", "msg" => $msg));
+                if (!$this->crud_util->hasErrors()) {
+                    $sql_string = "SELECT `id` FROM `message` WHERE `sender_id` = :sender_id AND `stamp` = :stamp";
+                    $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time));
+                    if (!$this->crud_util->hasErrors()) {
+                        $message_id = $this->crud_util->getFirstResult()["id"];
+                        $sql_string = "INSERT INTO `project_feedback_message`(`message_id`, `project_id`) VALUES (:message_id, :project_id)";
+                        $this->crud_util->execute($sql_string, array(
+                            "message_id" => $message_id, "project_id" => $project_id
+                        ));
+                        if (!$this->crud_util->hasErrors()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            // then add the message to the project messages table
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getProjectFeedbackMessages(string|int $project_id)
+    {
+        // get all the messages in the project feedback 
+        // get all the messages in a project forum
+        try {
+            // use a join between the messages table and the project table where ids are equal
+            try {
+                $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `project_feedback_message` WHERE `project_id` = :project_id)";
+                $this->crud_util->execute($sql_string, array("project_id" => $project_id));
+                if (!$this->crud_util->hasErrors()) {
+                    $this->message_data = $this->crud_util->getResults();
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
         } catch (\Exception $exception) {
             throw $exception;
         }
@@ -96,15 +148,55 @@ class ProjectLeader implements Model
     {
         // add the message to the project message table and the messages table
         try {
+            // add the message to the message table first
+            if (!empty($msg)) {
+                $sql_string = "INSERT INTO `message`(`sender_id`, `stamp`, `message_type`, `msg`) VALUES (:sender_id, :stamp, :message_type, :msg)";
+                $date_time = date('Y-m-d H:i:s');
+                $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time, "message_type" => "GROUP_FEEDBACK_MESSAGE", "msg" => $msg));
+                if (!$this->crud_util->hasErrors()) {
+                    $sql_string = "SELECT `id` FROM `message` WHERE `sender_id` = :sender_id AND `stamp` = :stamp";
+                    $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time));
+                    if (!$this->crud_util->hasErrors()) {
+                        $message_id = $this->crud_util->getFirstResult()["id"];
+                        $sql_string = "INSERT INTO `group_feedback_message`(`message_id`, `project_id`, `group_id`) VALUES (:message_id, :project_id, :group_id)";
+                        $this->crud_util->execute($sql_string, array("message_id" => $message_id, "project_id" => $project_id, "group_id" => $group_id));
+                        if (!$this->crud_util->hasErrors()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            // then add the message to the project messages table
         } catch (\Exception $exception) {
             throw $exception;
         }
     }
 
-    public function getProjectFeedbackMessages(string|int $project_id)
+    public function getGroupFeedbackMessages(string|int $project_id, string|int $group_id)
     {
-        // get all the messages in the project feedback 
+        // get all the messages in a project forum
         try {
+            // use a join between the messages table and the project table where ids are equal
+            try {
+                $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `group_feedback_message` WHERE `project_id` = :project_id AND `group_id` = :group_id)";
+                $this->crud_util->execute($sql_string, array("project_id" => $project_id));
+                if (!$this->crud_util->hasErrors()) {
+                    $this->message_data = $this->crud_util->getResults();
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
         } catch (\Exception $exception) {
             throw $exception;
         }
