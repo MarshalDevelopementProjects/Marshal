39e57c21b85cc972c64de6a5edc73a3671e4cab9
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-24 04:44:59 +0530

    test cases for the messaging methods of the group member model

---
+++ Tests/Test.Models/test.groupmember.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\Model\GroupMember;
+
+
+function testGetGroupData() {
+    $group_member = new GroupMember(1, 1);
+    echo "<pre>";
+    echo "Testing the getGroupData() method\n";
+    echo "<br>";
+    var_dump($group_member->getGroupData());
+    echo "</pre>";
+}
+
+function testSaveGroupMessage() {
+    $group_member = new GroupMember(1, 1);
+    echo "<pre>";
+    echo "Testing the saveGroupMessage() method\n";
+    $group_member->saveGroupMessage(1, 1, 1, "Hello my fellow group members");
+    echo "<br>";
+    echo "Passed the test for saveGroupMessages()";
+    echo "</pre>";
+}
+
+function testGetGroupMessages() {
+    $group_member = new GroupMember(1, 1);
+    echo "<pre>";
+    echo "Testing the getGroupMessages() method\n";
+    echo "<br>";
+    $group_member->getGroupMessages(1, 1);
+    var_dump($group_member->getMessageData());
+    echo "</pre>";
+}
+
+testGetGroupData();
+testSaveGroupMessage();
+testGetGroupMessages();
