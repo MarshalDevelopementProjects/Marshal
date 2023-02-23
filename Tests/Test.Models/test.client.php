bb5b279d20880fccc29ab33f3781899145ccd728
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-24 04:43:13 +0530

    added test cases for the client model

---
+++ Tests/Test.Models/test.client.php
@@ -0,0 +1,45 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\Model\Client;
+
+
+function testGetProjectData()
+{
+    $client = new Client(1);
+    echo "<pre>";
+    echo "Testing the getProjectData() method\n";
+    echo "<br>";
+    var_dump($client->getProjectData());
+    echo "</pre>";
+    echo "<pre>";
+}
+
+function testSaveProjectFeedbackMessage()
+{
+    echo "<pre>";
+    echo "Testing the saveProjectFeedbackMessage() method\n";
+    echo "<br>";
+    $client = new Client(1);
+    $client->saveProjectFeedbackMessage(1,  "Hello Project leaders");
+    echo "Passed the test\n";
+    echo "</pre>";
+}
+
+function testGetProjectFeedbackMessages()
+{
+    $client = new Client(1);
+    $client->getProjectFeedbackMessages();
+    echo "<pre>";
+    echo "Testing the getProjectFeedbackMessages() method\n";
+    echo "<br>";
+    var_dump($client->getMessageData());
+    echo "</pre>";
+}
+
+testGetProjectFeedbackMessages();
+testSaveProjectFeedbackMessage();
+
+testGetProjectFeedbackMessages();
+testGetProjectData();
