062e8aabf4c61703d3930de5c2f369f515d40801
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-25 17:07:04 +0530

    changed method signature and completed the messaging end point functions

--- App/Controller/GroupMember/GroupMemberController.php
+++ App/Controller/GroupMember/GroupMemberController.php
@@ -6,6 +6,7 @@ use App\Controller\Authenticate\UserAuthController;
 use App\Controller\ProjectMember\ProjectMemberController;
 use App\Model\GroupMember;
 use Core\Validator\Validator;
+use Exception;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
@@ -17,7 +18,12 @@ class GroupMemberController extends ProjectMemberController
     {
         try {
             parent::__construct();
-        } catch (\Exception $exception) {
+            if (array_key_exists("group_id", $_SESSION)) {
+                $this->groupMember = new GroupMember($_SESSION["project_id"], $_SESSION["group_id"]);
+            } else {
+                throw new Exception("Bad request missing arguments");
+            }
+        } catch (Exception $exception) {
             throw $exception;
         }
     }
@@ -36,5 +42,40 @@ class GroupMemberController extends ProjectMemberController
             view: "/group_member/groupInfo.html",
             status: "success",
         );
+    }
+
+    // $args must follow this format
+    // ["message" => "content of the message"]
+    public function postMessageToGroupForum(array $args)
+    {
+        try{
+            if (!empty($args) && array_key_exists("message", $args)) {
+                if (!empty($args["message"])) {
+                    if($this->groupMember->saveGroupMessage(id: $this->user->getUserData()->id, project_id: $_SESSION["project_id"], msg: $args["message"])) {
+                        $this->sendJsonResponse("success");
+                    } else {
+                        $this->sendJsonResponse("error", ["message" => "Message cannot be saved to the database"]);
+                    }
+                } else {
+                    $this->sendJsonResponse("error", ["message" => "Empty message body!"]);
+                }
+            } else {
+                $this->sendJsonResponse("error", ["message" => "Invalid request  format!"]);
+            }
+        } catch (Exception $exception) {
+            throw  $exception;
+        }
+    }
+
+    public function getGroupForumMessages() {
+        try{
+            if ($this->groupMember->getGroupMessages(project_id: $_SESSION["project_id"])) {
+                $this->sendJsonResponse("success", ["message" => "Successfully retrieved", "messages" => $this->groupMember->getMessageData() ?? []]);
+            } else {
+                $this->sendJsonResponse("error", ["message" => "Group is not valid"]);
+            }
+        } catch (Exception $exception) {
+            throw $exception;
+        }
     }
 }
