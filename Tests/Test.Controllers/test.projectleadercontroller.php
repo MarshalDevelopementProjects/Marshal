8cbdb94903d09ac2e7c3953568fcf8403c20063f
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-25 10:01:07 +0530

    changed the test cases to support the new design

---
+++ Tests/Test.Controllers/test.projectleadercontroller.php
@@ -0,0 +1,55 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\Controller\ProjectLeader\ProjectLeaderController;
+
+// before you run the test cases please make sure that you are logged in
+// otherwise you cannot execute the test cases
+// and make sure that you are logged in as kylo_ren when testing
+
+function testPostMessageToProjectForum(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->postMessageToProjectForum(["message" => "Hello fellow project members, how you all doing?"]);
+}
+
+function testGetProjectForumMessages(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->getProjectForumMessages();
+}
+
+function testPostMessageToProjectFeedback(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->postMessageToProjectFeedback(["message" => "Hello client how is your life?"]);
+}
+
+function testGetProjectFeedbackMessages(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->getProjectFeedbackMessages();
+}
+
+// the following two functions can only be tested much later
+/*function testPostMessageToGroupFeedback(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->postMessageToGroupFeedback(["message" => "Hello client how is your life?"]);
+}*/
+
+/*function testGetGroupFeedbackMessages(): void
+{
+    $project_leader_controller = new ProjectLeaderController();
+    $project_leader_controller->getGroupFeedbackMessages();
+}*/
+
+// uncomment a single test at a time
+
+// testPostMessageToProjectForum();
+// testGetProjectForumMessages();
+// testPostMessageToProjectFeedback();
+// testGetProjectFeedbackMessages();
+// testPostMessageToGroupFeedback();
+// testGetGroupFeedbackMessages();
