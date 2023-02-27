026df405b4f15e8153c869fb920c33d0781acb21
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-02-27 06:50:46 +0530

    changed get messages sql_string to a new query to get the profile picture of the sender and added new methods(get and post) for project task feedbacks

--- App/Model/ProjectMember.php
+++ App/Model/ProjectMember.php
@@ -10,7 +10,7 @@ class ProjectMember implements Model
 {
     private CrudUtil $crud_util;
     private array|object $project_data;
-    private array|object $message_data;
+    private array|object|null $message_data = null;
 
     public function __construct(string|int $project_id)
     {
@@ -24,6 +24,12 @@ class ProjectMember implements Model
             throw $exception;
         }
     }
+
+    /*
+     *
+        SELECT m.*, u.profile_picture FROM message m JOIN user u ON m.sender_id = u.id JOIN project_message pm ON m.id = pm.message_id WHERE pm.project_id = [YOUR_PROJECT_ID] ORDER BY m.stamp;
+     *
+     * */
 
     public function saveForumMessage(string|int $id, string $msg): bool
     {
@@ -66,7 +72,8 @@ class ProjectMember implements Model
         try {
             // use a join between the messages table and the project table where ids are equal
             try {
-                $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `project_message` WHERE `project_id` = :project_id)";
+                // $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `project_message` WHERE `project_id` = :project_id)";
+                $sql_string  = "SELECT m.*, u.`profile_picture` AS `sender_profile_picture` FROM `message` m JOIN `user` u ON m.`sender_id` = u.`id` JOIN `project_message` pm ON m.`id` = pm.`message_id` WHERE pm.`project_id` = :project_id ORDER BY m.stamp";
                 $this->crud_util->execute($sql_string, array("project_id" => $this->project_data->id));
                 if (!$this->crud_util->hasErrors()) {
                     $this->message_data = $this->crud_util->getResults();
@@ -101,6 +108,59 @@ class ProjectMember implements Model
             $result = $this->crud_util->execute($sql_string, $args);
             if ($result->getCount() > 0) {
                 $this->project_data = $result->getFirstResult(); // get all the results or just one result this is an array of objects
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function saveProjectTaskFeedbackMessage(string|int $id, string|int $task_id, string $msg): bool
+    {
+        try {
+            // add the message to the message table first
+            if (!empty($msg)) {
+                $sql_string = "INSERT INTO `message`(`sender_id`, `stamp`, `message_type`, `msg`) VALUES (:sender_id, :stamp, :message_type, :msg)";
+                $date_time = date('Y-m-d H:i:s');
+                $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time, "message_type" => "TASK_FEEDBACK_MESSAGE", "msg" => $msg));
+                if (!$this->crud_util->hasErrors()) {
+                    $sql_string = "SELECT `id` FROM `message` WHERE `sender_id` = :sender_id AND `stamp` = :stamp AND `message_type` = 'TASK_FEEDBACK_MESSAGE'";
+                    $this->crud_util->execute($sql_string, array("sender_id" => $id, "stamp" => $date_time));
+                    if (!$this->crud_util->hasErrors()) {
+                        $message = $this->crud_util->getFirstResult();
+                        $sql_string = "INSERT INTO `project_task_feedback_message`(`message_id`, `project_id`, `task_id`) VALUES (:message_id, :project_id, :task_id)";
+                        $this->crud_util->execute($sql_string, array("message_id" => $message->id, "project_id" => $this->project_data->id, "task_id" => $task_id));
+                        if (!$this->crud_util->hasErrors()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            // then add the message to the project messages table
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getProjectTaskFeedbackMessages(string|int $task_id): bool
+    {
+        // get all the messages in a project forum
+        try {
+            // $sql_string = "SELECT * FROM `message` WHERE `id` in (SELECT `message_id` FROM `project_task_feedback_message` WHERE `project_id` = :project_id AND `task_id` = :task_id)";
+            $sql_string = "SELECT m.*, u.`profile_picture` AS `sender_profile_picture` FROM `message` m JOIN `user` u ON m.`sender_id` = u.`id` JOIN `project_task_feedback_message` ptfm ON m.`id` = ptfm.`message_id` WHERE ptfm.`project_id` = :project_id AND ptfm.`task_id` = :task_id ORDER BY m.stamp";
+            $this->crud_util->execute($sql_string, array("project_id" => $this->project_data->id, "task_id" => $task_id));
+            if (!$this->crud_util->hasErrors()) {
+                $this->message_data = $this->crud_util->getResults();
                 return true;
             } else {
                 return false;
