c8e9b5216e43e9075a997eb0f54f0d50817d2632
TDMWalpala <tharindudarshana2019@gmail.com>
2023-02-27 14:02:57 +0530

    Added js for display the error msg

--- View/js/admin/login.js
+++ View/js/admin/login.js
@@ -1,1 +1,9 @@
 console.log(jsonData);
+const ErrorDiv = document.getElementById("error");
+const ErrorMSG = document.getElementById("error_msg");
+
+ErrorMSG.innerHTML = `${jsonData.errors}`;
+console.log(`${jsonData.errors}`);
+if(ErrorMSG.innerHTML != '' && ErrorMSG.innerHTML !='undefined'){
+    ErrorDiv.classList.add("active");
+}
