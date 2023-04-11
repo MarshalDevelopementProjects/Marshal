d19ab221dbc110a45c070f9cb03250f7d370956a
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-04-11 17:59:42 +0530

    completed the message forum methods for the project leader and the co

--- View/js/group_leader/forum.js
+++ View/js/group_leader/forum.js
@@ -1,8 +1,12 @@
+const MessageContainerDiv = document.getElementById('messages-container-div');
+const MessageInputForm = document.getElementById('message-input-form');
 console.log(jsonData);
 let projectID = jsonData.project_id;
 let groupID = jsonData.group_id;
 let userData = jsonData.user_data;
 let date = new Date();
+
+document.body.onload = async () => { await onStartUp();};
 
 // For today's date;
 Date.prototype.today = function () {
@@ -47,6 +51,8 @@ groupLeaderForumConnection.onmessage = (event) => {
         if(message_data.profile_picture !== undefined) console.log(message_data.profile_picture);
         if(message_data.date_time!== undefined) console.log(message_data.date_time);
         if(message_data.message !== undefined)console.log(message_data.message);*/
+        if(message_data.sender_username !== undefined)
+            appendMessage('IN', MessageContainerDiv, message_data);
     }
     onMessage(event.data);
 }
@@ -71,9 +77,17 @@ async function onStartUp() {
         if (response.ok) {
             let data = response.json();
             // TODO: ATTACH THE MESSAGES TO THE FORUM
-            if (data.length > 0) {
-                data.forEach(
-                    message => console.log(message)
+            if (data.messages.length > 0) {
+                data.messages.forEach(
+                    message => {
+                        console.log(message)
+                        // check whether the messages are incoming messages or outgoing messages
+                        if (message.sender_username !== userData.username) {
+                            appendMessage('IN', MessageContainerDiv, message);
+                        } else {
+                            appendMessage('OUT', MessageContainerDiv, message);
+                        }
+                    }
                 );
             } else {
                 console.log("No messages to display")
@@ -100,6 +114,16 @@ function closeConnection() {
 //      group_id: "If this is a group message need the task id"
 //      message: "BODY MESSAGE "
 // }
+
+MessageInputForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    let formObj = Object.fromEntries(new FormData(MessageInputForm));
+    if (formObj.message !=="") {
+        await sendMessages(formObj.message);
+    }
+    MessageInputForm.reset();
+});
+
 async function sendMessages(msg) {
     // TODO: ATTACH THE MESSAGE TO THE MESSAGING FORUM
 
@@ -107,6 +131,7 @@ async function sendMessages(msg) {
     msgObj.date_time = `${date.today()} ${date.timeNow()}`;
     // TODO: SEND THE MESSAGE
     groupLeaderForumConnection.send(JSON.stringify(msgObj));
+    appendMessage('OUT', MessageContainerDiv, msgObj);
     console.log(msgObj);
 
     // TODO: SEND THE MESSAGE TO THE APPROPRIATE END POINT(ASYNC)
@@ -130,4 +155,44 @@ async function sendMessages(msg) {
         console.error(error);
         groupLeaderForumConnection.close();
     }
+}
+
+function appendMessage(type, parent_div, message) {
+
+    let message_div = document.createElement('div'); // message
+
+    if (type === 'OUT') {
+        message_div.setAttribute('class', 'outgoing-message');
+    } else if (type === 'IN') {
+        message_div.setAttribute('class', 'incoming-message');
+    } else {
+        console.error('NOT A VALID MESSAGE TYPE');
+        message_div = undefined;
+        return;
+    }
+
+    let sender_details = document.createElement('div'); // sender details div
+    sender_details.setAttribute('class', 'sender-details');
+
+    let sender_profile_picture = document.createElement('img'); // sender profile picture img tag
+    sender_profile_picture.src = message.sender_profile_picture;
+
+    let sender_username = document.createElement('h5'); // sender user name heading
+    sender_username.innerText = message.sender_username;
+
+    let date_time = document.createElement('p'); // date time paragraph tag
+    date_time.innerText = message.stamp;
+
+    let message_content = document.createElement('p'); // message content
+    message_content.setAttribute('class', 'message-content');
+    message_content.innerText = message.msg;
+
+    // adding elements
+    sender_details.appendChild(sender_profile_picture);
+    sender_details.appendChild(sender_username);
+    sender_details.appendChild(date_time);
+
+    message_div.appendChild(sender_details);
+    message_div.appendChild(message_content);
+    parent_div.insertAdjacentElement("afterbegin", message_div);
 }
