aee664149b342e7c258f206f49047236e2127dd0
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-04-27 05:54:58 +0530

    new model for conferencing

--- App/Model/Conference.php
+++ App/Model/Conference.php
@@ -5,9 +5,11 @@ namespace App\Model;
 use App\CrudUtil\CrudUtil;
 use \Exception;
 
-class Meeting
+class Conference
 {
     private CrudUtil $crud_util;
+    private array|object|null $data = null;
+
     public function __construct()
     {
         try{
@@ -18,15 +20,163 @@ class Meeting
         }
     }
 
-    // TODO: Decide the format of the data being sent
     // TODO: Add the newly scheduled meeting to the notification table as well
-    public function scheduleMeeting(array $args) {
+    public function scheduleConference(array $args): bool
+    {
+        if (!empty($args)) {
+            try {
+                $sql_string = "INSERT INTO `conference`(
+                         `conf_name`,
+                         `project_id`,
+                         `leader_id`,
+                         `client_id`,
+                         `on`,
+                         `at`)
+                          VALUES (:conf_name, :project_id, :leader_id, :client_id, :on, :at)";
+                $this->crud_util->execute($sql_string, $args);
+                if(!$this->crud_util->hasErrors()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (Exception $exception) {
+                throw $exception;
+            }
+        } else {
+            return false;
+        }
     }
 
-
-    // TODO: QUERY THE SAME TABLE BUT WITH DIFFERENT IDS
     // TODO: ONLY FOR THE CLIENT AND THE PROJECT LEADER
     // TODO: PERHAPS EXTEND FOR GROUP MEMBERS AS WELL
-    public function getScheduledMeetings(string|int $id) {
+    public function getScheduledConferences(string|int $id, string $role): bool
+    {
+        try{
+            if ($role === "LEADER" ) {
+                $role = "leader_id";
+            } else if ($role === "CLIENT") {
+                $role = "client_id";
+            } else {
+                return false;
+            }
+            /*
+             * Example query string :
+               SELECT
+                    u.`profile_picture` AS `caller_dp`,
+                    u.`username` AS `caller_username`,
+                    p.`project_name`AS `project_name`,
+                    c.`on` AS `scheduled_date`,
+                    c.`at` AS `scheduled_time`,
+                    c.`status` AS `meeting_status`
+               FROM `conference`c
+               JOIN `user` u ON c.`leader_id` = u.`id`
+               JOIN `project` p ON p.`id` = c.`project_id`
+               WHERE c.`leader_id` = 1;
+            */
+            $sql_string = "SELECT
+                                c.`conf_id` AS `conf_id`,
+                                u.`profile_picture` AS `caller_dp`,
+                                u.`username` AS `caller_username`,
+                                p.`project_name`AS `project_name`,
+                                c.`on` AS `scheduled_date`,
+                                c.`at` AS `scheduled_time`,
+                                c.`status` AS `meeting_status`
+                           FROM `conference` c
+                               JOIN `user` u ON c.`" . $role . "` = u.`id`
+                               JOIN `project` p ON p.`id` = c.`project_id`
+                               WHERE c.`" . $role . "` = :id";
+            $this->crud_util = $this->crud_util->execute($sql_string, array("id" => $id));
+            if (!$this->crud_util->hasErrors()) {
+               $this->data = $this->crud_util->getResults();
+               return true;
+            } else {
+                $this->data = NULL;
+                return false;
+            }
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getDetailsOfConference(string|int $conf_id): array|bool
+    {
+        if ($conf_id) {
+            $sql_string = "SELECT * FROM `conference` WHERE `conf_id` = :id";
+            try {
+                $this->crud_util = $this->crud_util->execute($sql_string, array("id" => $conf_id));
+                if (!$this->crud_util->hasErrors()) {
+                    $this->data = $this->crud_util->getFirstResult();
+                    return true;
+                } else {
+                   return false;
+                }
+            } catch (Exception $exception) {
+                throw $exception;
+            }
+        } return false;
+    }
+
+    public function getScheduledConferencesByProject(int|string $id, int|string $project_id, string $role): bool|array
+    {
+        try{
+            if ($role === "LEADER" ) {
+                $role = "leader_id";
+            } else if ($role === "CLIENT") {
+                $role = "client_id";
+            } else {
+                return false;
+            }
+            $sql_string = "SELECT
+                                c.`conf_id` AS `conf_id`,
+                                u.`profile_picture` AS `caller_dp`,
+                                u.`username` AS `caller_username`,
+                                p.`project_name`AS `project_name`,
+                                c.`on` AS `scheduled_date`,
+                                c.`at` AS `scheduled_time`,
+                                c.`status` AS `meeting_status`
+                           FROM `conference` c
+                               JOIN `user` u ON c.`" . $role . "` = u.`id`
+                               JOIN `project` p ON p.`id` = c.`project_id`
+                               WHERE c.`" . $role . "` = :id AND c.`project_id` = " . ":project_id";
+            $this->crud_util = $this->crud_util->execute($sql_string, array("id" => $id, "project_id" => $project_id));
+            if (!$this->crud_util->hasErrors()) {
+                $this->data = $this->crud_util->getResults();
+                return true;
+            } else {
+                $this->data = NULL;
+                return false;
+            }
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function changeStatusOfConference(string|int $conf_id, string $status):bool|array
+    {
+        if ($conf_id && ($status === "PENDING" || $status === "DONE" || $status === "OVERDUE" || $status === "CANCELLED")) {
+           try {
+               if ($this->getDetailsOfConference($conf_id) && $this->data->status !== "CANCELLED") {
+                   $sql_string = "UPDATE `conference` SET `status` = :status WHERE `conf_id` = :conf_id";
+                   $this->crud_util->execute($sql_string, array(
+                       "status" => $status,
+                       "conf_id" => $conf_id
+                   ));
+                   if (!$this->crud_util->hasErrors()) {
+                       return true;
+                   }
+               } else {
+                   return ["Status of a cancelled conference that was scheduled prior can't be changed"];
+               }
+               return false;
+           } catch (Exception $exception) {
+               throw $exception;
+           }
+        }
+        return false;
+    }
+
+   public function getConferenceData(): array|object|null
+    {
+        return $this->data;
     }
 }
