c24b886329d546f03fd4bd9b23394e276494b33d
Harsha Gunawardane <mrgunawardane@gmail.com>
2023-04-27 11:07:19 +0530

    add file uploded feature

---
+++ App/Model/File.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Model;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use App\CrudUtil\CrudUtil;
+
+class File implements Model
+{
+    private CrudUtil $crud_util;
+    public function __construct()
+    {
+        try {
+            $this->crud_util = new CrudUtil();
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getFiles($condition){
+        $sql = "SELECT * FROM `files` WHERE ";
+        $sql .= $condition;
+
+        try {
+            $result = $this->crud_util->execute($sql);
+            if ($result->getCount() > 0) {
+                return $result->getResults();
+            } else {
+                return array();
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+}
---
+++ View/js/project_member/file_uploader.js
@@ -0,0 +1,58 @@
+console.log(jsonData)
+const uploadedFiles = document.querySelector('.uploadedfiles');
+
+var uploadFileCode = ""
+
+if(jsonData){
+    jsonData.forEach(file => {
+        uploadFileCode += `<a href="${file['filePath']}" target="_blank" style="text-decoration: none; "text-style: none; color: black; >
+                                <div class="uploadedfile">
+                                    <img src="${file['profile']}" alt="dp">
+                                    <div class="uploadedFileDetails">
+                                        <p class="fileName">${file['fileName']}</p>
+                                        <div class="uploaderDetails">
+                                            <p class="uploderName">${file['uploaderName']}</p>
+                                            <p class="uploadedDate">${file['date']}</p>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </a>`
+    });
+}
+uploadedFiles.innerHTML = uploadFileCode
+
+const fileUploadForm = document.getElementById("upload-file-form");
+
+fileUploadForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const inputFile = document.getElementById("uploadedfile");
+
+    let formData = new FormData();
+    let file = inputFile.files[0];
+    formData.append("uploadedfile", file);
+    try {
+        let response = await fetch("http://localhost/public/projectmember/fileupload", {
+            withCredentials: true,
+            credentials: "include",
+            mode: "cors",
+            method: "POST",
+            body: formData,
+            headers: {
+                "FILE_TYPE": file.type,
+                "FILE_NAME": file.name
+            }
+        });
+        // make this a pop up
+        let message = (await response.json()).message;
+        if (response.ok) {
+            console.log(message);
+            location.reload();
+        } else {
+            console.log(message);
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+});
