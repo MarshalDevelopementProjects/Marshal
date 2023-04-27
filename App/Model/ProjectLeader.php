48d7c04abb6ca5afd94386b2b275ecc2c5c4a86a
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-04-27 05:56:15 +0530

    the project leader now support the required functionalities of conferencing

--- App/Model/ProjectLeader.php
+++ App/Model/ProjectLeader.php
@@ -11,6 +11,7 @@ class ProjectLeader implements Model
 
     private CrudUtil $crud_util;
     private array|object $project_data;
+    private array|object $project_member_data;
     private array|object|null $message_data = null;
 
     public function __construct(string|int $project_id)
@@ -25,6 +26,229 @@ class ProjectLeader implements Model
             throw $exception;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function saveProjectFeedbackMessage(string|int $id, string $msg): bool
     {
@@ -276,6 +500,56 @@ class ProjectLeader implements Model
         } catch (\Exception $exception) {
             throw $exception;
         }
+    }
+
+    public function getProjectMembersByRole(string|int $project_id, string $role): bool
+    {
+       if ($project_id && $role) {
+           try {
+               // get the details of the clients
+               $sql_string = "SELECT u.id AS id, u.username AS username, u.profile_picture AS profile_picture, p_j.role AS role FROM project_join p_j JOIN user u ON p_j.member_id = u.id WHERE p_j.project_id = :project_id AND p_j.role = :role";
+               $this->crud_util = $this->crud_util->execute($sql_string, ["project_id" => $project_id, "role" => $role]);
+               if(!$this->crud_util->hasErrors()) {
+                   $this->project_data = $this->crud_util->getResults();
+                   return true;
+               } else {
+                   return false;
+               }
+           } catch (\Exception $exception) {
+               throw  $exception;
+           }
+       }
+       return false;
+    }
+
+    public function getProjectMembers(): bool
+    {
+        try {
+            $sql_string = "SELECT `user`.`username` AS `username`,
+                           `user`.user_status AS `status`,
+                           `user`.user_state AS `state`,
+                           `user`.profile_picture AS `profile_picture`,
+                           `project_join`.role AS `role`
+                            FROM `user`
+                            INNER JOIN
+                           `project_join` ON
+                           `project_join`.`project_id` = :project_id AND 
+                           `user`.`id` = `project_join`.`member_id`";
+            $this->crud_util->execute($sql_string, ["project_id" => $this->project_data->id]);
+            if (!$this->crud_util->hasErrors()) {
+                $this->project_member_data = $this->crud_util->getResults();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function getProjectMemberData(): array|null|object|bool
+    {
+        return $this->project_member_data;
     }
 
     public function getProjectData(): object|array|null|bool
