2ee6abcaa6342468d3933ded2eb3d25bb4c60ebd
tharindu darshana <tdmw2k19@gmail.com>
2023-04-29 10:21:21 +0530

    Added separart js file for notificationPopup event listener

---
+++ View/js/user/notificationPopup.js
@@ -0,0 +1,9 @@
+const notificationPopupBtn = document.querySelector('.notification-bell-btn')
+const notificationPopup = document.querySelector('.notification-popup-container');
+const notificationPopupCloseBtn = document.querySelector('.notification-popup-close-btn');
+const notificationPopupContainer = document.querySelector('.notification-popup-container');
+const container = document.querySelector('.container');
+
+notificationPopupBtn.addEventListener('click', () => notificationPopup.classList.add('active'));
+notificationPopupCloseBtn.addEventListener('click', () => notificationPopup.classList.remove('active'));
+notificationPopup.addEventListener('click', () => notificationPopup.classList.remove('active'))
