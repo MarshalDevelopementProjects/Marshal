ce14440251a74318b067e83f39d8da7e484d0427
TDMWalpala <tharindudarshana2019@gmail.com>
2023-05-03 15:19:53 +0530

    Commented js that already inthe sidebar.js

--- View/js/project_member/getProjectInfo.js
+++ View/js/project_member/getProjectInfo.js
@@ -14,57 +14,57 @@ notificationPopup.addEventListener('click', () => notificationPopup.classList.re
 
 
 
-const dashboard = document.querySelector('.dashboard');
-const newProject = document.querySelector('.new-project');
-const profile = document.querySelector('.profile');
-const settings = document.querySelector('.settings');
-const sketchIdea = document.querySelector('.sketch-idea');
+// const dashboard = document.querySelector('.dashboard');
+// const newProject = document.querySelector('.new-project');
+// const profile = document.querySelector('.profile');
+// const settings = document.querySelector('.settings');
+// const sketchIdea = document.querySelector('.sketch-idea');
 
-dashboard.addEventListener('click', function(){
-    dashboard.classList.add('active');
-    profile.classList.remove('active');
-    newProject.classList.remove('active');
-    settings.classList.remove('active');
-    sketchIdea.classList.remove('active');
+// dashboard.addEventListener('click', function(){
+//     dashboard.classList.add('active');
+//     profile.classList.remove('active');
+//     newProject.classList.remove('active');
+//     settings.classList.remove('active');
+//     sketchIdea.classList.remove('active');
 
-})
-newProject.addEventListener('click', function(){
-    dashboard.classList.remove('active');
-    profile.classList.remove('active');
-    newProject.classList.add('active');
-    settings.classList.remove('active');
-    sketchIdea.classList.remove('active');
+// })
+// newProject.addEventListener('click', function(){
+//     dashboard.classList.remove('active');
+//     profile.classList.remove('active');
+//     newProject.classList.add('active');
+//     settings.classList.remove('active');
+//     sketchIdea.classList.remove('active');
 
-    createProjectPopup.classList.add('active');
+//     createProjectPopup.classList.add('active');
 
-})
-profile.addEventListener('click', function(){
-    dashboard.classList.remove('active');
-    profile.classList.add('active');
-    newProject.classList.remove('active');
-    settings.classList.remove('active');
-    sketchIdea.classList.remove('active');
+// })
+// profile.addEventListener('click', function(){
+//     dashboard.classList.remove('active');
+//     profile.classList.add('active');
+//     newProject.classList.remove('active');
+//     settings.classList.remove('active');
+//     sketchIdea.classList.remove('active');
 
 
-})
-settings.addEventListener('click', function(){
-    dashboard.classList.remove('active');
-    profile.classList.remove('active');
-    newProject.classList.remove('active');
-    settings.classList.add('active');
-    sketchIdea.classList.remove('active');
+// })
+// settings.addEventListener('click', function(){
+//     dashboard.classList.remove('active');
+//     profile.classList.remove('active');
+//     newProject.classList.remove('active');
+//     settings.classList.add('active');
+//     sketchIdea.classList.remove('active');
 
-})
+// })
 
-sketchIdea.addEventListener('click', function(){
-    dashboard.classList.remove('active');
-    profile.classList.remove('active');
-    newProject.classList.remove('active');
-    settings.classList.remove('active');
-    sketchIdea.classList.add('active');
+// sketchIdea.addEventListener('click', function(){
+//     dashboard.classList.remove('active');
+//     profile.classList.remove('active');
+//     newProject.classList.remove('active');
+//     settings.classList.remove('active');
+//     sketchIdea.classList.add('active');
 
-    window.location.href = "./index.html";
-})
+//     window.location.href = "./index.html";
+// })
 
 
 // set the groups
