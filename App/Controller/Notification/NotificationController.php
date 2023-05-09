d29f28aca9a3f7963a85fe74a3d87ddff8d70b61
Harsha Gunawardane <mrgunawardane@gmail.com>
2023-05-09 15:21:47 +0530

    Write unit test cases for notification controller

--- App/Controller/Notification/NotificationController.php
+++ App/Controller/Notification/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Notification;
 
 use App\Model\Notification;
+use App\Model\Project;
 
 class NotificationController{
     
@@ -20,10 +21,37 @@ class NotificationController{
 
         // var_dump($args);
         $notification = new Notification();
-        
+
         try {
+
+            // check if there is any missing arguments
+            $requiredKeys = array('message', 'type', 'sender_id', 'url');
+
+            foreach ($requiredKeys as $key) {
+                if (!isset($args[$key]) || empty($args[$key])) {
+                    throw new \Exception('Missing argument: ' . $key);
+                }
+            }
+
             $date = date("Y-m-d H:i:s");
             $projectId = $_SESSION['project_id'];
+
+            // check sender id is valid or not
+            $project = new Project();
+
+            if(!$project->readUserRole(member_id: $args['sender_id'], project_id: $_SESSION['project_id'])){
+                throw new \Exception("Sender with ID ". $args['sender_id'] ." does not exist");
+            }
+
+            // check reciver id is valid or not
+            if($args['recive_id'] != null && !$project->readUserRole(member_id: $args['recive_id'], project_id: $_SESSION['project_id'])){    
+                throw new \Exception("Invalid argument type for recive id");
+            }
+
+            // check URL
+            if (strpos($args['url'], "http://localhost/public") !== 0) {
+                throw new \Exception("Invalid URL format");
+            }
 
             $notificationArgs = array(
                 "project_id" => $projectId,
@@ -60,14 +88,28 @@ class NotificationController{
     public function boardcastNotification(int $notificationId, array $members):bool{
 
         $notification = new Notification();
-        try {
-            foreach($members as $member){
-                // var_dump($member);
-                $notification->setNotifiers(array("notification_id" => $notificationId, "member_id" => $member->member_id), array("notification_id", "member_id"));
+        $project = new Project();
+
+        if($notification->getNotification(array("id" => $notificationId), array("id")) != array()){
+            if($members){
+                try {
+                    foreach($members as $member){
+                        // var_dump($member->member_id);
+                        if(!$project->readUserRole(member_id: $member->member_id, project_id: $_SESSION['project_id'])){
+                            return false;
+                        }
+                        $notification->setNotifiers(array("notification_id" => $notificationId, "member_id" => $member->member_id), array("notification_id", "member_id"));
+                    }
+                    return true;
+                } catch (\Throwable $th) {
+                    return false;
+                }
+            }else{
+                return false;
             }
-            return true;
-        } catch (\Throwable $th) {
+        }else{
             return false;
         }
+        
     }
 }
