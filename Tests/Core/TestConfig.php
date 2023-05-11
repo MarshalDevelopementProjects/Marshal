fdebc1a29b0930942422e94d799f47550b4d5be0
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-05-11 08:55:46 +0530

    new test files and classes for the core

--- Tests/Core/TestConfig.php
+++ Tests/Core/TestConfig.php
@@ -1,8 +1,50 @@
 <?php
 
-namespace Core;
+namespace Tests\Core;
 
-class TestConfig
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Core\Config;
+use Tests\Tester;
+
+class TestConfig extends Tester
 {
 
+    private Config $config;
+
+    public function __construct()
+    {
+        parent::__construct(Config::class);
+    }
+
+    protected function setup(): void
+    {
+        $this->config =  new Config();
+    }
+
+    public function testGetApiGlobalWithNullValue(): void
+    {
+        $this->assertNull($this->config::getApiGlobal(null));
+    }
+
+    public function testGetApiGlobalWithInvalidKey(): void
+    {
+        $this->assertNull($this->config::getApiGlobal(key: "new"));
+    }
+
+    public function testGetApiGlobalWithValidKey(): void
+    {
+        $this->assertTrue(is_array($this->config::getApiGlobal(key: "remember")));
+        $this->assertTrue(is_array($this->config::getApiGlobal(key: "mysql")));
+    }
+
+    public function run(): void
+    {
+        // TODO: Implement run() method.
+        $this->setup();
+        $this->testGetApiGlobalWithNullValue();
+        $this->testGetApiGlobalWithInvalidKey();
+        $this->testGetApiGlobalWithValidKey();
+        $this->summary();
+    }
 }
--- Tests/Core/TestValidator.php
+++ Tests/Core/TestValidator.php
@@ -1,8 +1,104 @@
 <?php
 
-namespace Core;
+namespace Tests\Core;
 
-class TestValidator
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Core\Validator\Validator;
+use Tests\Tester;
+
+class TestValidator extends Tester
 {
+    private Validator $validator;
+    public function __construct()
+    {
+        parent::__construct(unit_name: Validator::class);
+    }
 
+    protected function setup(): void
+    {
+        $this->validator = new Validator();
+    }
+
+    public function testGetPassedBeforeRunningValidations(): void
+    {
+        $this->assertFalse($this->validator->getPassed());
+    }
+
+    public function testValidateWithEmptySchema():void
+    {
+        $this->assertException(
+            callback: Validator::class . "::validate",
+            args: [
+                "values" => [
+                    "username" => "ed_north",
+                    "email_address" => "edward_north@gmail.com",
+                    "password" => "1234567890",
+                ],
+                "schema" => ""
+            ]
+        );
+    }
+
+    public function testValidateWithEmptyValues():void
+    {
+
+        $this->validator->validate(values: [], schema: "login");
+        $this->assertFalse($this->validator->getPassed());
+    }
+
+    public function testValidatorWithValidTestData(): void
+    {
+        $this->validator->validate(
+            values: [
+                "username" => "kylo_ren",
+                "email_address" => "kylo_ren@gmail.com",
+                "password" => "1234567890",
+            ],
+            schema: "login"
+        );
+        $this->assertTrue($this->validator->getPassed());
+    }
+
+    public function testGetPassedAfterRunningValidValidations(): void
+    {
+        $this->assertTrue($this->validator->getPassed());
+    }
+
+    public function testValidatorWithInvalidTestData(): void
+    {
+        $this->validator->validate(
+            values: [
+                "username" => "ed_seaborn",
+                "email_address" => "edward_seaborn@gmail.com",
+                "password" => "1234567890",
+            ],
+            schema: "login"
+        );
+        $this->assertFalse($this->validator->getPassed());
+    }
+
+    public function testGetPassedAfterRunningInvalidValidations(): void
+    {
+        $this->assertFalse($this->validator->getPassed());
+    }
+
+    public function run(): void
+    {
+        // TODO: Implement run() method.
+        $this->setup();
+
+        $this->testGetPassedBeforeRunningValidations();
+
+        $this->testValidateWithEmptySchema();
+
+        $this->testValidateWithEmptyValues();
+
+        $this->testValidatorWithValidTestData();
+        $this->testGetPassedAfterRunningValidValidations();
+
+        $this->testValidatorWithInvalidTestData();
+        $this->testGetPassedAfterRunningInvalidValidations();
+        $this->summary();
+    }
 }
