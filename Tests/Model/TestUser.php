30eccfbddc187618b3760b2ff640581e0d724c9f
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-05-12 09:03:17 +0530

    added new model test class for user model

---
+++ Tests/Model/TestUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Model;
+
+use App\Model\User;
+use Tests\Tester;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class TestUser extends Tester
+{
+
+    private User $user;
+
+    public function __construct()
+    {
+        parent::__construct(User::class);
+    }
+
+    protected function setup(): void
+    {
+       $this->user = new User();
+    }
+
+    public function testCreateUserWithEmptyArgs(): void
+    {
+        $this->assertFalse($this->user->createUser(args: []));
+    }
+
+    public function testCreateUserWithMissingArguments():void
+    {
+        $this->assertException(callback: User::class . '::createUser', args: [
+                "args" => [
+                    "username" => "kylo_ren"
+                ]
+            ],
+            message: ""
+        );
+    }
+
+    public function run(): void
+    {
+        // TODO: Implement run() method.
+        $this->setup();
+
+        $this->testCreateUserWithEmptyArgs();
+
+        $this->testCreateUserWithMissingArguments();
+
+        $this->summary();
+    }
+}
--- Tests/run_script.php
+++ Tests/run_script.php
@@ -4,9 +4,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Tests\Core\TestValidator;
 use Tests\Core\TestConfig;
+use Tests\Model\TestUser;
 
 $testValidator = new TestValidator();
 $testValidator->run();
 
 $testConfig = new TestConfig();
 $testConfig->run();
+
+$testUserModel = new TestUser();
+$testUserModel->run();
